bfb7200d6b5f9c0c06adfa2c417862e56b580a03
Jean Ferri <jeanferrimh@gmail.com>
2022-04-10 17:12:17 -0300

    Testes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 var teste = Array.from(document.querySelectorAll('.livro__link')).map(function( elemento , indice, arrayBase){
     const imagem = (elemento.querySelector('.livro__img').src)
     const titulo = (elemento.querySelector('.livro__titulo').innerHTML)
@@ -6,3 +9,55 @@ return { imagem: imagem, titulo: titulo, autor: autor}
 }
    
 )
+
+
+//animação dos itens surgindo
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+        target.forEach(function(element){
+        if(windowTop > element.offsetTop){
+            element.classList.add(animationClass)
+        } else {
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll()
+
+if(target.length) {
+window.addEventListener('scroll', debounce(function(){
+    animeScroll()
+    console.log('aasas')
+}, 100))
+}
+
+
+// teste adicionar imagem
+
+const img = document.getElementById('imagem')
+const botao = document.getElementById('botaoImg')
+const imgNova = document.getElementById('imgNova')
+console.log(img.)
+function addimg(){
+    
+}
