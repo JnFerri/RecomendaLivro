fcbfd37f55521eaf9b49be30447859ba615b8369
Jean Ferri <jeanferrimh@gmail.com>
2022-04-12 13:26:32 -0300

    12/04/2022

--- adicionarLivro/Livro.js
+++ adicionarLivro/Livro.js
@@ -1,3 +1,5 @@
+
+
 export class Livro {
     static numeroLivros = 0
     imagem;
@@ -13,8 +15,43 @@ export class Livro {
     autor = this.autor
     descricao = this.descricao
     Livro.numeroLivros += 1
+
+    
+
 }
 }
+
+
+
+function adicionarNovoLivro(){
+    
+    var imagem = document.getElementById("imagemPost").value
+     var nomeLivro = document.getElementById("nomeLivro").value
+     var nomeAutor = document.getElementById("nomeAutor").value
+     var descricao = document.getElementById("descricaoLivro").value 
+ 
+ 
+    const informacoes = { 
+        imagem: imagem,
+        nome: nomeLivro,
+        autor: nomeAutor,
+        descricao:descricao
+                                 }  
+ 
+         console.log(informacoes)
+ 
+ 
+    const livro = new Livro(informacoes.imagem, informacoes.nome, informacoes.autor, informacoes.descricao)
+ 
+ 
+                                                                
+ console.log(livro)
+ 
+    
+ 
+ }
+
+console.log(Livro)
 
 
 
--- adicionarLivro/index.js
+++ adicionarLivro/index.js
@@ -1,31 +1,50 @@
-import {Livro} from "./adicionarLivro/Livro.js"
 
-var botao = document.getElementById('botaoAddNovoLivro')
-botao.onclick(adicionarLivro())
-function adicionarLivro(){
+var LivrosArray=[]
+
+
+
+function adicionarNovoLivro(){
+    
    var imagem = document.getElementById("imagemPost").value
     var nomeLivro = document.getElementById("nomeLivro").value
     var nomeAutor = document.getElementById("nomeAutor").value
     var descricao = document.getElementById("descricaoLivro").value 
 
 
-   const informacoes = { 
+    LivrosArray.push( { 
        imagem: imagem,
        nome: nomeLivro,
        autor: nomeAutor,
        descricao:descricao
                                 }  
-
-        console.log(informacoes)
-
-
-   const livro = [new Livro(informacoes.imagem, informacoes.nome, informacoes.autor, informacoes.descricao)]
+    )
 
 
-                                                               
 
-                                
+
+
+}
+
+class Livro {
+   static numeroLivros = 0
+   imagem;
+   nome;
+   autor;
+   descricao;
+
+
+
+   constructor(imagem , nome , autor, descricao){
+   imagem = this.imagem
+   nome = this.nome
+   autor = this.autor
+   descricao = this.descricao
+   Livro.numeroLivros += 1
 
    
 
 }
+}
+
+
+console.log(livro)
