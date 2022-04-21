7af27bd45bf067c45b485025c85f633a5815fd4f
Jean Ferri <jeanferrimh@gmail.com>
2022-04-21 13:15:53 -0300

    footer

---
+++ assets/js/footer.js
@@ -0,0 +1,9 @@
+const footer = document.querySelector('.footer')
+
+window.onload = function footer(){
+    footer.innerHTML = `<div class="footer__container">
+                    <img class="footer__img" src="assets/img/logo.png" alt=" logo recomenda livros">
+                    
+                    <h3 class="footer__contato">Contato: jeanferrimh@gmail.com</h3>
+                </div>`
+}
